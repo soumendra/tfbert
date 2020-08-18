976eb74ce7c3923c86d1d1c941728de5eae10148
adimyth <mishraaditya6991@gmail.com>
2020-08-18 22:41:51 +0530

    adds seperate script for creating dataset

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ from attrdict import AttrDict  # type: ignore
 
 configs_local = {
     "random_state": 42,
-    "max_len": 64,
-    "train_batch_size": 32,
-    "eval_batch_size": 8,
-    "epochs": 3,
+    "max_len": 48,
+    "train_batch_size": 4,
+    "eval_batch_size": 2,
+    "epochs": 4,
     "lr": 3e-5,
-    "model_name": "bert-base-uncased",
+    "model_name": "distilbert-base-uncased",
     "datapath": "data/cola_public_1.1/cola_public/raw/",
 }
 
--- tfbert/cola_data.py
+++ tfbert/cola_data.py
@@ -1,55 +1,12 @@
+from .dataset import BertDataset
+import numpy as np
 import pandas as pd  # type: ignore
 from pathlib import Path
 from pandas import DataFrame
 from typing import List, Union
 from tensorflow.keras.utils import to_categorical  # type: ignore
 import tensorflow_addons as tfa  # type: ignore
-from transformers import AutoTokenizer  # type: ignore
-import tensorflow as tf  # type: ignore
 from attrdict import AttrDict  # type: ignore
-
-tf.get_logger().setLevel("INFO")
-
-
-tokenizer = BertTokenizerFast("data/bert-base-uncased-vocab.txt", lowercase=True)
-
-
-def preprocess(sentence, label, max_len):
-    sentence = str(sentence.decode("utf-8"))
-    sentence = " ".join(sentence.split())
-    encoded = tokenizer.encode_plus(
-        sentence,
-        add_special_tokens=True,
-        max_length=max_len,
-        pad_to_max_length=True,
-        return_attention_mask=True,
-        truncation=True,
-    )
-    input_ids = encoded["input_ids"]
-    attention_mask = encoded["attention_mask"]
-    return (input_ids, attention_mask, label)
-
-
-class BertDataset(tf.data.Dataset):
-    def _generator(sentences, labels, max_len):
-        for sent, lbl in zip(sentences, labels):
-            yield preprocess(sent, lbl, max_len)
-
-    def __new__(cls, sentences, labels, max_len):
-        return tf.data.Dataset.from_generator(
-            cls._generator,
-            args=(sentences, labels, max_len),
-            output_types=(tf.dtypes.int32, tf.dtypes.int32, tf.dtypes.float32),
-            output_shapes=((max_len,), (max_len,), (2,)),
-        )
-
-    @staticmethod
-    def create(sentences, labels, max_len, batch_size):
-        dataset = BertDataset(sentences, labels, max_len)
-        dataset = dataset.cache()
-        dataset = dataset.batch(batch_size)
-        dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
-        return dataset
 
 
 class ColaData:
@@ -59,14 +16,14 @@ class ColaData:
 
     @staticmethod
     def get_cola_xy(df: DataFrame) -> List[DataFrame]:
-        return [df["sentence"], df["label"]]
+        return [df["sentence"].head(50), df["label"].head(50)]
 
     def get_cola_df(self):
         in_domain_train = pd.read_csv(self.path / "in_domain_train.tsv", sep="\t", names=self.cols)
         in_domain_val = pd.read_csv(self.path / "in_domain_dev.tsv", sep="\t", names=self.cols)
         out_domain_val = pd.read_csv(self.path / "out_of_domain_dev.tsv", sep="\t", names=self.cols)
         val = in_domain_val.append(out_domain_val)
-        test = pd.read_csv(self.path / "../../../cola_out_of_domain_test.tsv", sep="\t")
+        test = pd.read_csv(self.path / "../../../cola_out_of_domain_test.tsv", sep="\t")[:10]
         return [in_domain_train, val, test]
 
     def __init__(self, path: Union[str, Path]):
@@ -89,8 +46,11 @@ class ColaData:
         self.loss_fn = loss_fn
         self.optimizer = optimizer
 
-        self.train_dataset = BertDataset.create(self.x_train, self.y_train_enc, config.max_len, config.train_batch_size)
-        self.val_dataset = BertDataset.create(self.x_val, self.y_val_enc, config.max_len, config.eval_batch_size)
+        train_data_obj = BertDataset(config.max_len, config.model_name, config.train_batch_size)
+        self.train_dataset = train_data_obj.create(self.x_train.values, self.y_train_enc)
+
+        valid_data_obj = BertDataset(config.max_len, config.model_name, config.eval_batch_size)
+        self.valid_dataset = valid_data_obj.create(self.x_val.values, self.y_val_enc)
 
         self.model.compile(
             optimizer=self.optimizer,
@@ -98,22 +58,13 @@ class ColaData:
             metrics=["accuracy", tfa.metrics.MatthewsCorrelationCoefficient(num_classes=2)],
         )
 
-        self.model.fit(
-            self.train_dataset,
-            epochs=config.epochs,
-            validation_data=self.val_dataset,
-            steps_per_epoch=len(self.x_train) // config.train_batch_size,
-        )
+        self.model.fit(self.train_dataset, epochs=config.epochs, validation_data=self.valid_dataset)
         return self
 
     def create_submission(self):
-        self.test_dataset = BertDataset.create(
-            self.testdf["Sentence"].values,
-            [[0, 1]] * len(self.testdf),  # creating fake labels
-            self.config.max_len,
-            self.config.eval_batch_size,
-        )
+        test_data_obj = BertDataset(self.config.max_len, self.config.model_name, self.config.eval_batch_size)
+        self.test_dataset = test_data_obj.create(self.testdf["Sentence"].values, [[0, 1]] * len(self.testdf))
         preds = self.model.predict(self.test_dataset)
-        self.testdf["Label"] = preds
+        self.testdf["Label"] = np.argmax(preds, axis=1)
         print(f"\n\nTest Data: \n{self.testdf['Label'].value_counts()}")
         self.testdf[["Id", "Label"]].to_csv("sample_submission.csv", index=False)
