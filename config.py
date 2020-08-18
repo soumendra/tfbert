b9c2d371147c73efca5313680c13ea7d1b81b860
adimyth <mishraaditya6991@gmail.com>
2020-08-19 01:19:42 +0530

    makes changes to config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ configs_local = {
     "max_len": 64,
     "train_batch_size": 32,
     "eval_batch_size": 8,
-    "epochs": 4,
+    "epochs": 3,
     "lr": 3e-5,
     "model_name": "bert-base-uncased",
     "datapath": "data/cola_public_1.1/cola_public/raw/",
